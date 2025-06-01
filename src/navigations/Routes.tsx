c80708ae386590bb7cfb30fa7fa889c9df8eef65
Yogesh Kumar <yogeshkumar@Yogeshs-MacBook-Air.local>
2025-06-02 00:32:23 +0530

    minor changes

--- src/navigations/Routes.tsx
+++ src/navigations/Routes.tsx
@@ -1,13 +1,9 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import MainStack from './Mainstack';
 import AuthStack from './Authstack';
 import { useSelector } from 'react-redux';
 import { RootState } from '../Redux/store';
-
-const Stack = createNativeStackNavigator();
 
 const Routes = () => {
   const user = useSelector((state: RootState) => state.userDetail);
@@ -19,5 +15,3 @@ const Routes = () => {
 };
 
 export default Routes;
-
-const styles = StyleSheet.create({});
